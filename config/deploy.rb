506ba945b03ea6016ea16473e900b73876f6b056
MikeIT-JEC <mike.jec.it@gmail.com>
2016-11-17 19:04:52 +0800

    ad

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,17 @@ lock '3.6.1'
 set :application, 'studynihongo'
 set :repo_url, 'git://github.com/MikeIT-JEC/studynihongo.git'  
 
+set :user, "deploy"
+set :stage, :production 
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, 'home/deploy/studynihongo' 
  
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_files, %w{config/database.yml config/secrets.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default value for :scm is :git
 # set :scm, :git
 
@@ -29,7 +32,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
  # set :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
- # set :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+ set :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
