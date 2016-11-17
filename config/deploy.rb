69bd982587a7dab1fb58291c76567230df5b4d6d
MikeIT-JEC <mike.jec.it@gmail.com>
2016-11-17 18:58:40 +0800

    ad

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git://github.com/MikeIT-JEC/studynihongo.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, 'home/deploy/studynihongo' 
+set :deploy_to, 'home/deploy/studynihongo' 
  
 # set :linked_files, %w{config/database.yml config/secrets.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
